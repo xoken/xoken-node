d17ce84595d75bacfb283fbec88b3d605bdaa2a8
tinqnit <tinqnit@gmail.com>
2019-11-17 14:39:51 +0530

    BlockSync core connection setup functions

--- node/src/Network/Xoken/Node/P2P/BlockSync.hs
+++ node/src/Network/Xoken/Node/P2P/BlockSync.hs
@@ -8,6 +8,7 @@
 
 module Network.Xoken.Node.P2P.BlockSync
     ( createSocket
+    , setupPeerConnection
     ) where
 
 import Control.Concurrent.MVar
@@ -16,55 +17,68 @@ import Control.Monad.Logger
 import Control.Monad.Reader
 import qualified Data.ByteString.Lazy as BSL
 import Data.Int
+import Data.Maybe
 import Data.Serialize
 import Data.String.Conversions
 import Data.Text (Text)
+import Data.Time.Clock
+import Data.Time.Clock.POSIX
 import Data.Word
 import Network.Socket
 import qualified Network.Socket.ByteString.Lazy as N (sendAll)
 import Network.Xoken.Constants
+import Network.Xoken.Network.Common
 import Network.Xoken.Node.Env
 import Network.Xoken.Node.P2P.Types
 import Network.Xoken.P2P.Common
+import System.Random
 
--- import Data.ByteString (ByteString)
--- import qualified Data.ByteString as B
 --
 -- | Eg: createSocket "127.0.0.1" 3000 TCP
-createSocket :: String -> Int -> IO Socket
+createSocket :: AddrInfo -> IO Socket
 createSocket = createSocketWithOptions []
 
-createSocketWithOptions :: [SocketOption] -> String -> Int -> IO Socket
-createSocketWithOptions options ip port =
-    withSocketsDo $ do
-        let portNo = Just (show port)
-        let transport_type = Stream
-        let hints = defaultHints {addrSocketType = transport_type}
-        addr:_ <- getAddrInfo (Just hints) (Just ip) portNo
-        sock <- socket AF_INET transport_type (addrProtocol addr)
-        mapM_ (\option -> when (isSupportedSocketOption option) (setSocketOption sock option 1)) options
-        connect sock (addrAddress addr)
-        return sock
+createSocketWithOptions :: [SocketOption] -> AddrInfo -> IO Socket
+createSocketWithOptions options addr = do
+    sock <- socket AF_INET Stream (addrProtocol addr)
+    mapM_ (\option -> when (isSupportedSocketOption option) (setSocketOption sock option 1)) options
+    connect sock (addrAddress addr)
+    return sock
 
-sendMessage :: MVar Bool -> Socket -> BSL.ByteString -> IO ()
-sendMessage writeLock sock msg = do
+sendEncMessage :: MVar Bool -> Socket -> BSL.ByteString -> IO ()
+sendEncMessage writeLock sock msg = do
     a <- takeMVar writeLock
     N.sendAll sock msg
     putMVar writeLock a
 
-setupConnectionPeer :: (HasService env m) => SockAddr -> m ()
-setupConnectionPeer sa = do
+setupPeerConnection :: (HasService env m) => m ()
+setupPeerConnection = do
     bp2pEnv <- asks getBitcoinP2PEnv
     let bnConfig = bitcoinNodeConfig bp2pEnv
-    liftIO $ print ("setupConnectionPeer: ")
-    let x = bncNet bnConfig
-    -- ManagerConfig {mgrConfNetAddr = ad, mgrConfNetwork = net} <- asks myConfig
-    -- let ad = NetworkAddress 0 (SockAddrInet 0 0)
-    -- nonce <- liftIO randomIO
-    -- bb <- 0 :: Word32 -- getBestBlock
-    -- now <- round <$> liftIO getPOSIXTime
-    -- let rmt = NetworkAddress (srv net) sa
-    --     ver = buildVersion (bncNet bnConfig) nonce bb ad rmt now
-    -- b <- asks onlinePeers
-    -- _ <- atomically $ newOnlinePeer b sa nonce p a
+        net = bncNet bnConfig
+        seeds = getSeeds net
+        hints = defaultHints {addrSocketType = Stream}
+        port = getDefaultPort net
+    let sd = map (\x -> Just (x :: HostName)) seeds
+    addrs <- liftIO $ mapM (\x -> head <$> getAddrInfo (Just hints) (x) (Just (show port))) sd
+    liftIO $
+        mapM_
+            (\y -> do
+                 z <- (createSocket y)
+                 doVersionHandshake bnConfig z (addrAddress y))
+            addrs
+
+doVersionHandshake :: BitcoinNodeConfig -> Socket -> SockAddr -> IO ()
+doVersionHandshake bnConfig sock sa = do
+    liftIO $ print ("setupPeerConnection: ")
+    g <- liftIO $ getStdGen
+    now <- round <$> liftIO getPOSIXTime
+    let nonce = fst (random g :: (Word64, StdGen))
+        ad = NetworkAddress 0 (SockAddrInet 0 0)
+        bb = 0 :: Word32 -- ### TODO: getBestBlock ###
+        rmt = NetworkAddress 0 sa
+        ver = buildVersion (bncNet bnConfig) nonce bb ad rmt now
+        em = encode ver
+    mv <- (newMVar True)
+    sendEncMessage mv sock (BSL.fromStrict em)
     return ()
