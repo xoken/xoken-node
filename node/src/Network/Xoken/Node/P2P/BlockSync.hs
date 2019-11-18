1d09d7312bbff17a342b5a2195d093167ef0c214
tinqnit <tinqnit@gmail.com>
2019-11-18 19:31:37 +0530

    socket IO exception handling

--- node/src/Network/Xoken/Node/P2P/BlockSync.hs
+++ node/src/Network/Xoken/Node/P2P/BlockSync.hs
@@ -5,12 +5,14 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Network.Xoken.Node.P2P.BlockSync
     ( createSocket
     , setupPeerConnection
     ) where
 
+import Control.Concurrent.Async (mapConcurrently)
 import Control.Concurrent.MVar
 import Control.Monad
 import Control.Monad.Logger
@@ -25,41 +27,52 @@ import Data.Text (Text)
 import Data.Time.Clock
 import Data.Time.Clock.POSIX
 import Data.Word
+
+-- import GHC.Exception.Type hiding (Exception)
+import Control.Exception
 import Network.Socket
+
 import qualified Network.Socket.ByteString as S (recv)
 import qualified Network.Socket.ByteString.Lazy as N (recv, sendAll)
 import Network.Xoken.Constants
-
-import Control.Concurrent.Async
-import Network.Xoken.Network.Common
+import Network.Xoken.Network.Common (MessageCommand(..), NetworkAddress(..))
 import Network.Xoken.Network.Message
 import Network.Xoken.Node.Env
 import Network.Xoken.Node.P2P.Types
 import Network.Xoken.P2P.Common
 import System.Random
 
+-- import qualified UnliftIO.Exception as UE (bracket, throwIO, try)
 --
 -- | Eg: createSocket "127.0.0.1" 3000 TCP
-createSocket :: AddrInfo -> IO Socket
+createSocket :: AddrInfo -> IO (Maybe Socket)
 createSocket = createSocketWithOptions []
 
-createSocketWithOptions :: [SocketOption] -> AddrInfo -> IO Socket
+-- createSocketWithOptions :: [SocketOption] -> AddrInfo -> IO (Maybe Socket)
+-- createSocketWithOptions options addr = do
+--     sock <- socket AF_INET Stream (addrProtocol addr)
+--     mapM_ (\option -> when (isSupportedSocketOption option) (setSocketOption sock option 1)) options
+--     connect sock (addrAddress addr) `catch` (\(err :: IOException) -> putStrLn ("caught: " ++ show err))
+--     return $ Just sock
+createSocketWithOptions :: [SocketOption] -> AddrInfo -> IO (Maybe Socket)
 createSocketWithOptions options addr = do
-    liftIO $ print ("createSocketWithOptions: ")
     sock <- socket AF_INET Stream (addrProtocol addr)
     mapM_ (\option -> when (isSupportedSocketOption option) (setSocketOption sock option 1)) options
-    connect sock $ addrAddress addr
-    return sock
+    res <- try $ connect sock (addrAddress addr)
+    case res of
+        Right () -> return $ Just sock
+        Left (e :: IOException) -> do
+            print ("TCP socket connect fail: " ++ show (addrAddress addr))
+            return Nothing
 
 sendEncMessage :: MVar Bool -> Socket -> BSL.ByteString -> IO ()
 sendEncMessage writeLock sock msg = do
     a <- takeMVar writeLock
-    N.sendAll sock msg
+    (N.sendAll sock msg) `catch` (\(e :: IOException) -> putStrLn ("caught: " ++ show e))
     putMVar writeLock a
 
 setupPeerConnection :: (HasService env m) => m ()
 setupPeerConnection = do
-    liftIO $ print ("setupPeerConnection: ")
     bp2pEnv <- asks getBitcoinP2PEnv
     let bnConfig = bitcoinNodeConfig bp2pEnv
         net = bncNet bnConfig
@@ -68,42 +81,47 @@ setupPeerConnection = do
         port = getDefaultPort net
     liftIO $ print (show seeds)
     let sd = map (\x -> Just (x :: HostName)) seeds
-    addrs <- liftIO $ mapM (\x -> head <$> getAddrInfo (Just hints) (x) (Just (show port))) sd
-    liftIO $ print (show addrs)
-    liftIO $
-        mapM_
-            (\y ->
-                 async
-                     (do sock <- createSocket y
-                         doVersionHandshake bnConfig sock $ addrAddress y))
+    addrs <- liftIO $ mapConcurrently (\x -> head <$> getAddrInfo (Just hints) (x) (Just (show port))) sd
+    res <-
+        liftIO $
+        mapConcurrently
+            (\y -> do
+                 sock <- createSocket y
+                 case sock of
+                     Just sx -> doVersionHandshake bnConfig sx $ addrAddress y
+                     Nothing -> return False)
             addrs
+    return ()
 
 readNextMessage :: Socket -> Network -> IO (Maybe MessageCommand)
 readNextMessage sock net = do
-    hdr <- S.recv sock 24
-    print ("recieved (header): " ++ show hdr)
-    case (decode hdr) of
-        Left _ -> do
-            print "Error decoding incoming message header"
-            return Nothing
-        Right (MessageHeader _ _ len _) -> do
-            print ("length: " ++ show len)
-            byts <-
-                if len == 0
-                    then return hdr
-                    else do
-                        y <- S.recv sock (fromIntegral len)
-                        print ("recieved ....... " ++ show y)
-                        return $ hdr `B.append` y
-            case runGet (getMessage net) $ byts of
-                Left e -> do
-                    print "Error, unexpected message header"
+    res <- try $ (S.recv sock 24)
+    case res of
+        Right hdr -> do
+            case (decode hdr) of
+                Left _ -> do
+                    print "Error decoding incoming message header"
                     return Nothing
-                Right msg -> return $ Just (msgType msg)
+                Right (MessageHeader _ _ len _) -> do
+                    byts <-
+                        if len == 0
+                            then return hdr
+                            else do
+                                rs <- try $ (S.recv sock (fromIntegral len))
+                                case rs of
+                                    Left (e :: IOException) -> throw e
+                                    Right y -> return $ hdr `B.append` y
+                    case runGet (getMessage net) $ byts of
+                        Left e -> do
+                            print "Error, unexpected message header"
+                            return Nothing
+                        Right msg -> return $ Just (msgType msg)
+        Left (e :: IOException) -> do
+            print ("socket read fail")
+            return Nothing
 
 doVersionHandshake :: BitcoinNodeConfig -> Socket -> SockAddr -> IO (Bool)
 doVersionHandshake bnConfig sock sa = do
-    liftIO $ print ("doVersionHandshake: ")
     g <- liftIO $ getStdGen
     now <- round <$> liftIO getPOSIXTime
     myaddr <- head <$> getAddrInfo (Just defaultHints {addrSocketType = Stream}) (Just "192.168.0.106") (Just "3000")
@@ -115,18 +133,16 @@ doVersionHandshake bnConfig sock sa = do
         ver = buildVersion net nonce bb ad rmt now
         em = runPut . putMessage net $ (MVersion ver)
     mv <- (newMVar True)
-    print (show em)
     sendEncMessage mv sock (BSL.fromStrict em)
     hs1 <- readNextMessage sock net
     case hs1 of
         Just MCVersion -> do
-            print "Got [Version]"
             hs2 <- readNextMessage sock net
             case hs2 of
                 Just MCVerAck -> do
-                    print "Got [VerAck]"
                     let em2 = runPut . putMessage net $ (MVerAck)
                     sendEncMessage mv sock (BSL.fromStrict em2)
+                    print ("Version handshake complete: " ++ show sa)
                     return True
                 __ -> do
                     print "Error, unexpected message (2) during handshake"
@@ -134,13 +150,3 @@ doVersionHandshake bnConfig sock sa = do
         __ -> do
             print "Error, unexpected message (1) during handshake"
             return False
-        -- Right (MessageHeader _ cmd len _) -> do
-        --     case cmd of
-        --         MCVersion -> do
-        --             print "Got [Version]"
-        --         MCVerAck -> do
-        --             print "Got [VerAck]"
-        --             return True
-        --         __ -> do
-        --             print "Error, unexpected message header"
-        --             return False
