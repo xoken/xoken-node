523b996257d88ee858bef7601f45940eac83095a
Aravind Virat <aravind.mallapureddy@juspay.in>
2020-05-13 15:43:48 +0530

    added endpoint url and port to create config

--- node/app/Main.hs
+++ node/app/Main.hs
@@ -173,6 +173,8 @@ defaultConfig path = do
                 20
                 5
                 3
+                "127.0.0.1"
+                9090
     Config.makeConfig config (path <> "/arivi-config.yaml")
 
 makeGraphDBResPool :: IO (ServerState)
