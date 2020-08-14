dcddf09e7de57acc9241095b7531d6ad85002f16
Arjun Khandkar <khandkararjun@gmail.com>
2020-08-14 10:19:57 +0530

    batch & window size set to 4

--- node/src/Network/Xoken/Node/P2P/BlockSync.hs
+++ node/src/Network/Xoken/Node/P2P/BlockSync.hs
@@ -174,7 +174,7 @@ runEgressBlockSync =
                  let staleTime = fromInteger $ fromIntegral (unresponsivePeerConnTimeoutSecs $ nodeConfig bp2pEnv)
                  case recvtm of
                      Just rt -> do
-                         if (fw < 50) && (diffUTCTime tm rt < staleTime)
+                         if (fw < 4) && (diffUTCTime tm rt < staleTime)
                              then do
                                  mmsg <- produceGetDataMessage tm
                                  case mmsg of
@@ -213,7 +213,7 @@ runEgressBlockSync =
                                              atomically $ modifyTVar' (bitcoinPeers bp2pEnv) (M.delete (bpAddress peer))
                                      else liftIO $ threadDelay (100000)
                              Nothing -> do
-                                 if (fw < 50)
+                                 if (fw < 4)
                                      then do
                                          mmsg <- produceGetDataMessage tm
                                          case mmsg of
@@ -301,7 +301,7 @@ checkBlocksFullySynced conn = do
             return False
 
 getBatchSize :: Int32 -> Int32 -> [Int32]
-getBatchSize peerCount n = [1 :: Int32 .. 100]
+getBatchSize peerCount n = [1 :: Int32 .. 4]
 
 getNextBlockToSync :: (HasXokenNodeEnv env m, HasLogger m, MonadIO m) => UTCTime -> m (Maybe BlockInfo)
 getNextBlockToSync tm = do
