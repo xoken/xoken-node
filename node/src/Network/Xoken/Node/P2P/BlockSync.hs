8808279a9486df20e5b58f2e56da203a241b602c
stoichammer <tinqnit@gmail.com>
2020-09-23 15:32:47 +0530

    uncomment txOutputValuesCache

--- node/src/Network/Xoken/Node/P2P/BlockSync.hs
+++ node/src/Network/Xoken/Node/P2P/BlockSync.hs
@@ -635,11 +635,11 @@ processConfTransaction tx bhash blkht txind = do
     inputs <-
         mapM
             (\(b, j) -> do
-                 tuple <- return Nothing
-                    --  liftIO $
-                    --  TSH.lookup
-                    --      (txOutputValuesCache bp2pEnv)
-                    --      (getTxShortHash (outPointHash $ prevOutput b) (txOutputValuesCacheKeyBits $ nodeConfig bp2pEnv))
+                 tuple <-
+                     liftIO $
+                     TSH.lookup
+                         (txOutputValuesCache bp2pEnv)
+                         (getTxShortHash (outPointHash $ prevOutput b) (txOutputValuesCacheKeyBits $ nodeConfig bp2pEnv))
                  val <-
                      case tuple of
                          Just (ftxh, indexvals) ->
@@ -724,12 +724,11 @@ processConfTransaction tx bhash blkht txind = do
                      , (a, (txHashToHex $ TxHash $ sha256 (scriptOutput o)), fromIntegral $ outValue o)))
                 outAddrs
     trace lg $ LG.msg $ "processing Tx " ++ show txhs ++ ": compiled output value(s): " ++ (show ovs)
-    -- liftIO $
-    --     TSH.insert
-    --         (txOutputValuesCache bp2pEnv)
-    --         (getTxShortHash (txHash tx) (txOutputValuesCacheKeyBits $ nodeConfig bp2pEnv))
-    --         (txHash tx, ovs)
-    --
+    liftIO $
+        TSH.insert
+            (txOutputValuesCache bp2pEnv)
+            (getTxShortHash (txHash tx) (txOutputValuesCacheKeyBits $ nodeConfig bp2pEnv))
+            (txHash tx, ovs)
     trace lg $ LG.msg $ "processing Tx " ++ show txhs ++ ": added outputvals to cache"
     -- update outputs and scripthash tables
     mapM_
