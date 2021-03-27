a45a072d0e950b50bc01d40afd3deb6a7d821295
Arjun Khandkar <khandkararjun@gmail.com>
2021-03-27 12:48:25 +0530

    fix empty list exception

--- node/src/Network/Xoken/Node/P2P/BlockSync.hs
+++ node/src/Network/Xoken/Node/P2P/BlockSync.hs
@@ -971,7 +971,13 @@ getSatsValueFromOutpoint conn txSync lg net outPoint wait maxWait confirmedOnly 
     res <- liftIO $ try $ query conn (Q.RqQuery $ Q.Query qstr par)
     case res of
         Right results -> do
-            if (L.length results == 0) || (((\(_, _, _, bi) -> bi) $ head $ results) == ("", -1, -1) && confirmedOnly)
+            if (L.length results == 0) ||
+               ((\r ->
+                     case r of
+                         [] -> False
+                         (_, _, _, bi):_ -> bi == ("", -1, -1))
+                    results &&
+                confirmedOnly)
                 then do
                     debug lg $
                         LG.msg $
